b3251f323e5c70f5e07e9aaf4bef2fc23b0c664a
Kathryn <kathryn.newbould@anaplan.com>
2018-06-28 11:04:45 +0100

    Customising

--- src/App.js
+++ src/App.js
@@ -5,14 +5,9 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="info-wrapper">
+          <h1 className="App-title">Kathryn Newbould</h1>
+          <h3>Software Engineer, Podcast Host, Woman in STEM</h3>
       </div>
     );
   }
